c931b32ed54e355a00168eb3f123389498bfd219
Emmanuel Sotelo <emmanuelsotelo1@hotmail.com>
2019-10-04 04:31:15 -0300

    .

---
+++ src/ParaDebug/Debug.java
@@ -0,0 +1,16 @@
+package ParaDebug;
+
+public class Debug {
+
+	public static void main(String[] args) {
+		int miArreglo[] = new int [5];
+		miArreglo[5]=0;
+		
+		int sumaNotasExamenes=60;
+		int numeroExamenes=0;
+		int promedio=sumaNotasExamenes/numeroExamenes;	
+
+
+	}
+
+}
